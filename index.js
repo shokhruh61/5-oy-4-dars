1e621a761e90e735143e21cee70de537dcc44a18
shokhruh61 <shohruxmahmudov61@gmail.com>
2024-10-26 13:37:58 +0500

    js

--- index.js
+++ index.js
@@ -16,14 +16,14 @@
 //   console.log(factorial(5));
 //   console.log(factorial(6));
 
-function fibonaci(n) {
-    if (n == 0) return 0; 
-    if (n == 1) return 1; 
-    return fibonaci(n - 1) + fibonaci(n - 2);
-  }
+// function fibonaci(n) {
+//     if (n == 0) return 0; 
+//     if (n == 1) return 1; 
+//     return fibonaci(n - 1) + fibonaci(n - 2);
+//   }
 
-  console.log(fibonaci(5));
-  console.log(fibonaci(10));
+//   console.log(fibonaci(5));
+//   console.log(fibonaci(10));
 
 // calssga oid masalalar
 
